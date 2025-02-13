5d9f0e498f581289918e37ea2c7b9b28b1578aa5
Joseph Morales <Joseph@LDcompany.com>
2025-02-12 21:30:14 -0500

    initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,21 @@
+import Header from './Components/Header.jsx';
+import Footer from './Components/Footer.jsx'
+import Hero from './Components/Hero.jsx'
+import Services from './Components/Services.jsx'
+import WhyUs from './Components/WhyUs.jsx'
+import Reviews from './Components/Reviews.jsx'
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Hero />
+            <Services />
+            <WhyUs />
+            <Reviews />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
